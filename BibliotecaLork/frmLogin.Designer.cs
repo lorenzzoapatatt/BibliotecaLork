d519e352f0a3137424ffd98317c56b1c45b5c409
Lorenzzo Antonio Patatt <lorenzzoapatatt2209@gmail.com>
2025-08-08 19:54:54 -0300

    Revert "botando os buttons"
    This reverts commit 9bd07597cc530027feb39604bb373fc4d90ca773.

--- BibliotecaLork/frmLogin.Designer.cs
+++ BibliotecaLork/frmLogin.Designer.cs
@@ -81,6 +81,7 @@
             txtLogin.ShadowDecoration.CustomizableEdges = customizableEdges2;
             txtLogin.Size = new Size(448, 45);
             txtLogin.TabIndex = 1;
+            txtLogin.Enter += txtLogin_Enter;
             // 
             // txtSenha
             // 
@@ -107,6 +108,7 @@
             txtSenha.ShadowDecoration.CustomizableEdges = customizableEdges4;
             txtSenha.Size = new Size(448, 45);
             txtSenha.TabIndex = 2;
+            txtSenha.Enter += txtSenha_Enter;
             // 
             // guna2HtmlLabel1
             // 
