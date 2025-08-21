68e49b93162d72e6201f56f87fa4f4f2ccb57b0f
Lorenzzo Antonio Patatt <lorenzzoapatatt2209@gmail.com>
2025-08-21 19:44:53 -0300

    retirando os erros do relatorioLivro

--- BibliotecaLork/frmRelatorioLivro.cs
+++ BibliotecaLork/frmRelatorioLivro.cs
@@ -8,87 +8,87 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace BibliotecaLork
+namespace BibliotecaLork;
+
+public partial class frmRelatorioLivro : Form
 {
     RelatorioLivroEmprestado? relatorioLivroSelecionado;
-    public partial class frmRelatorioLivro : Form
+    public frmRelatorioLivro()
     {
-        public frmRelatorioLivro()
+        InitializeComponent();
+    }
+
+    private void BuscarRelatorio()
+    {
+
+    }
+
+    private void btnEditar_Click(object sender, EventArgs e)
+    {
+        var msg = new Guna.UI2.WinForms.Guna2MessageDialog();
+        msg.Icon = Guna.UI2.WinForms.MessageDialogIcon.Information;
+
+        if (relatorioLivroSelecionado != null)
         {
-            InitializeComponent();
+            var relatorioEditar = new frmRelatorioLivroCad(relatorioLivroSelecionado);
+            relatorioEditar.Show();
+            msg.Show("Relatório editado com sucesso!");
+            BuscarRelatorio();
+            relatorioLivroSelecionado = null;
         }
-
-        private void BuscarRelatorio()
+        else
         {
-
+            msg.Show("Selecione um relatório para editar.");
         }
+    }
+    private void btnSalvar_Click(object sender, EventArgs e)
+    {
+        var frmRelatorioLivroCad = new frmRelatorioLivroCad();
+        frmRelatorioLivroCad.ShowDialog();
+        BuscarRelatorio();
+    }
+    private void btnExcluir_Click(object sender, EventArgs e)
+    {
+        var msg = new Guna.UI2.WinForms.Guna2MessageDialog();
 
-        private void btnEditar_Click(object sender, EventArgs e)
+        msg.Icon = Guna.UI2.WinForms.MessageDialogIcon.Information;
+
+        if (relatorioLivroSelecionado != null)
         {
-            var msg = new Guna.UI2.WinForms.Guna2MessageDialog();
-            msg.Icon = Guna.UI2.WinForms.MessageDialogIcon.Information;
-
-            if (relatorioLivroSelecionado != null)
+            using (var bd = new LivrosDBContext())
             {
-                var relatorioEditar = new frmRelatorioLivroCad(relatorioLivroSelecionado);
-                relatorioEditar.Show();
-                msg.Show("Relatório editado com sucesso!");
+                bd.RelatorioLivroEmprestados.Remove(relatorioLivroSelecionado);
+                bd.SaveChanges();
+                msg.Show("Relatório excluído com sucesso!");
                 BuscarRelatorio();
                 relatorioLivroSelecionado = null;
             }
-            else
+        }
+        else
+        {
+            msg.Show("Selecione um relatório para excluir.");
+        }
+    }
+
+    private void dgvRelatorioLivro_CellClick(object sender, DataGridViewCellEventArgs e)
+    {
+        if (!dgvRelatorioLivro.Rows[e.RowIndex].IsNewRow)
+        {
+            var livroEmprestado = dgvRelatorioLivro.Rows[e.RowIndex].DataBoundItem as RelatorioLivroEmprestado;
+            if (livroEmprestado != null)
             {
-                msg.Show("Selecione um relatório para editar.");
+                relatorioLivroSelecionado = livroEmprestado;
             }
         }
-        private void btnSalvar_Click(object sender, EventArgs e)
-        {
-            var frmRelatorioLivroCad = new frmRelatorioLivroCad();
-            frmEmprestimoCad.ShowDialog();
-            BuscarRelatorio();
-        }
-        private void btnExcluir_Click(object sender, EventArgs e)
-        {
-            var msg = new Guna.UI2.WinForms.Guna2MessageDialog();
-            var.Icon = Guna.UI2.WinForms.MessageDialogIcon.Information;
+    }
 
-            if (relatorioLivroSelecionado != null)
-            {
-                using (var bd = new LivrosDBContext())
-                {
-                    bd.RelatorioLivroEmprestados.Remove(relatorioLivroSelecionado);
-                    bd.SaveChanges();
-                    msg.Show("Relatório excluído com sucesso!");
-                    BuscarRelatorio();
-                    relatorioLivroSelecionado = null;
-                }
-            }
-            else
-            {
-                msg.Show("Selecione um relatório para excluir.");
-            }
-        }
+    private void frmRelatorioLivro_Load(object sender, EventArgs e)
+    {
+        BuscarRelatorio();
+    }
 
-        private void dgvRelatorioLivro_CellClick(object sender, DataGridViewCellEventArgs e)
-        {
-            if (!dgvRelatorioLivro.Rows[e.RowIndex].IsNewRow)
-            {
-                var livroEmprestado = dgvRelatorioLivro.Rows[e.RowIndex].DataBoundItem as RelatorioLivroEmprestado;
-                if (livroEmprestado != null)
-                {
-                    relatorioLivroSelecionado = livroEmprestado;
-                }
-            }
-        }
-
-        private void frmRelatorioLivro_Load(object sender, EventArgs e)
-        {
-            BuscarRelatorio();
-        }
-
-        private void txtPesquisar_TextChanged(object sender, EventArgs e)
-        {
-            BuscarRelatorio();
-        }
+    private void txtPesquisar_TextChanged(object sender, EventArgs e)
+    {
+        BuscarRelatorio();
     }
 }
